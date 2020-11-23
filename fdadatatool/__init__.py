d230621a950961d0396a23820fa1a62a23ae3274
Jonathan Hildenbrand <jonathanhild@users.noreply.github.com>
2020-11-23 10:32:30 -0800

    clean up

--- fdadatatool/__init__.py
+++ fdadatatool/__init__.py
@@ -4,10 +4,3 @@
 # https://opensource.org/licenses/MIT
 
 from .data_factory import factory
-# from .citations import ICitationsBuilder
-# from .compliance_actions import IComplianceActionsBuilder
-# from .firm_profiles import IFirmProfileBuilder
-# from .import_refusals import IImportRefusalsBuilder
-# from .imports_summary import IImportsSummaryBuilder
-# from .inspections import IInspectionsBuilder
-from .recalls import IRecallBuilder
